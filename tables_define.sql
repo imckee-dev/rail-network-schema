5add85b69aafec3d255332aa99fd830f59071bfb
imckee-dev <mckeei@oregonstate.edu>
2025-07-24 19:24:40 -0700

    tables boilerplate

---
+++ tables_define.sql
@@ -0,0 +1,24 @@
+
+-- our main four
+
+CREATE TABLE Shipments (
+
+);
+
+CREATE TABLE Containers (
+    
+);
+
+CREATE TABLE RailYards (
+    
+);
+
+CREATE TABLE Trains (
+    
+);
+
+
+-- intersection table
+CREATE TABLE container_train ( -- one train to many containers, and/or many trains to one container
+    
+);

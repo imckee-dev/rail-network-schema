04699e67d4500709939502fb2a577f65760412a9
imckee-dev <mckeei@oregonstate.edu>
2025-07-31 21:19:12 -0700

    base DML to finish, I did relaly quick SELECT * for each table. TODO: insert, delete, update

---
+++ DML.sql
@@ -0,0 +1,22 @@
+-- Next: Data Manipulation Language (our required queries)
+
+
+-- SELECT for each table
+
+SELECT * FROM RailYards;
+
+SELECT * FROM Trains;
+
+SELECT * FROM Shipments;
+
+SELECT * FROM Containers;
+
+-- intersection table
+SELECT * FROM container_train;
+
+
+-- TODO: INSERT, UPDATE, and DELETE query
+    -- [DELETE record from container_train intersection table w/o compromising (no data anomaly)]
+
+
+
